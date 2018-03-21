37eed25b8c4b2dbdcbec6348f89ee32d2a99b536
Agnieszka Urbańska <agnieszka.urbanska@ymail.com>
2018-03-21 19:34:48 +0100

    "Trekking app 1.0"

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+
+//
+//     // google.maps.event.addListener(map, 'click', function(event) {
+//     //     addMarker(event.latLng, map);
+//     // });
+// }
+
+
+var map;
+var markers = [];
+var city = ["Świnoujście", "Miedzyzdroje", "Dziwnów", "Pustkowo", "Mrzeżyno"];
+var cityIndex = 0;
+var cityLabelIndex = 0;
+var cityLabel = ["Świnoujście", "Miedzyzdroje", "Dziwnów", "Pustkowo", "Mrzeżyno"];
+
+function initMap() {
+    var lat_lng = {lat: 53.91053, lng: 14.24712};
+    var markersLatLng = [
+         {lat: 53.91053, lng: 14.24712},
+         {lat: 53.92921, lng: 14.45097},
+         {lat: 54.02819, lng: 14.76691},
+         {lat: 54.06834, lng: 14.97019},
+         {lat: 54.13929, lng: 15.28098} ];
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: lat_lng,
+        map: map
+
+    });
+
+    // Adds a marker at the center of the map.
+    for (var i = 0; i < markersLatLng.length; i++) {
+        var lat_lng = new google.maps.LatLng(markersLatLng[i].lat,markersLatLng[i].lng);
+        addMarker(lat_lng);
+    }
+}
+
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+    var marker = new google.maps.Marker({
+        position: location,
+        label: city[cityIndex++ % city.length],
+        title: cityLabel[cityLabelIndex++ % cityLabel.length],
+
+        map: map
+    });
+//    markers.push(marker);
+
+}
+
+//slider z miejscowościami-podłączyć linki i własne info
+//header
+//footer
